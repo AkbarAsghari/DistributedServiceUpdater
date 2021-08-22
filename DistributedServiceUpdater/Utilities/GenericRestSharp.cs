6e5fe88b9a29b94d4f52e667a3785da2c6521ae4
Akbar Asghari <Asghari.A@PLN-ASGHARI-A>
2021-08-22 17:45:07 +0430

    Add GenericRestSharp

---
+++ DistributedServiceUpdater/Utilities/GenericRestSharp.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedServiceUpdater.Utilities
+{
+    
+    public class GenericRestSharp<T>
+    {
+        private RestClient client;
+        private RestRequest request;
+
+        public HttpStatusCode StatusCode { get; private set; }
+        public string Url { get; private set; }
+        public string ResponseContent { get; private set; }
+
+        public GenericRestSharp()
+        {
+            client = new RestClient();
+            client.Timeout = -1;
+
+            request = new RestRequest();
+        }
+        public GenericRestSharp<T> SetToken(string token, string header = "Authorization")
+        {
+            request.AddHeader(header, token);
+            return this;
+        }
+
+        public GenericRestSharp<T> SetUrl(string url)
+        {
+            Url = url;
+            client.BaseUrl = new Uri(url);
+            return this;
+        }
+
+        public GenericRestSharp<T> SetMethod(Method method)
+        {
+            request.Method = method;
+            return this;
+        }
+
+        public GenericRestSharp<T> SetBody(object body, string type = "application/json")
+        {
+            request.AddParameter(type, JsonConvert.SerializeObject(body), ParameterType.RequestBody);
+            return this;
+        }
+
+        public GenericRestSharp<T> SetQueryParameters(Dictionary<string, object> queryParameters)
+        {
+            client.BaseUrl = new Uri(client.BaseUrl?.ToString() 
+                + "?" 
+                + String.Join("&", queryParameters.Select(x => x.Key.ToString() + "=" + x.Value.ToString())));
+            return this;
+        }
+
+        public GenericRestSharp<T> SetParameters(Dictionary<string, object> queryParameters)
+        {
+            foreach (var parameter in queryParameters)
+                request.AddParameter(parameter.Key, parameter.Value);
+            return this;
+        }
+
+
+
+        public T Execute()
+        {
+            IRestResponse response = client.Execute(request);
+
+            StatusCode = response.StatusCode;
+            ResponseContent = response.Content;
+
+            T responseObj = default(T);
+
+            try
+            {
+                responseObj = JsonConvert.DeserializeObject<T>(response.Content);
+            }
+            catch { }
+
+            return responseObj;
+        }
+    }
+}
